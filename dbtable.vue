c5d78c475c20afa6a916b1b7655c3c7e21cbd61f
Zixin Cheng <crh1_0006a@outlook.com>
2019-06-01 23:54:07 +0800

    More

---
+++ dbtable.vue
@@ -0,0 +1,44 @@
+<template>
+    <div class="result">
+                <table class="table">
+                    <thead>
+                        <tr>
+                            <th>书名</th>
+                            <th>作者</th>
+                            <th>书号</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td scope="row">1</td>
+                            <td></td>
+                            <td></td>
+                        </tr>
+                        <tr>
+                            <td scope="row">2</td>
+                            <td></td>
+                            <td></td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+</template>
+<!--CSS support-->
+    <style src="css/main.css"></style>
+    <!--bootstrap CDN-->
+    <style src="https://cdn.bootcss.com/twitter-bootstrap/4.3.1/css/bootstrap.min.css" ></style>
+    <!--JS support-->
+    <!--vue CDN-->
+    <script src="https://cdn.jsdelivr.net/npm/vue"></script>
+    <!--bootstrap CDN-->
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdn.bootcss.com/twitter-bootstrap/4.3.1/js/bootstrap.min.js"></script>
+
+<script>
+    export default(){
+    data:function(){
+        
+    }
+}
+</script>
