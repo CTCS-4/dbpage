14ca7e714e32907962f2765477318463e3771771
Zixin Cheng <crh1_0006a@outlook.com>
2019-06-02 09:43:06 +0800

    testing environment

--- dbvue/src/router.js
+++ dbvue/src/router.js
@@ -12,8 +12,8 @@ export default new Router({
       component: Home
     },
     {
-      path: '/about',
-      name: 'about',
+      path: '/add',
+      name: 'add',
       // route level code-splitting
       // this generates a separate chunk (about.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
---
+++ dbvue/src/views/dbtable.vue
@@ -0,0 +1,31 @@
+<template>
+    <div class="result">
+        <table class="table">
+            <thead>
+                <tr>
+                    <th>书名</th>
+                    <th>作者</th>
+                    <th>书号</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td scope="row">1</td>
+                    <td></td>
+                    <td></td>
+                </tr>
+                <tr>
+                    <td scope="row">2</td>
+                    <td></td>
+                    <td></td>
+                </tr>
+            </tbody>
+        </table>
+    </div>
+</template>
+
+<script>
+export default {
+    name:'dbtable'
+}
+</script>
